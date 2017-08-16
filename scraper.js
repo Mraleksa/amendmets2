3ddf1c89f5d95f0aaa63551128e48fc9f305e32f
Mraleksa <a.gribanovskiy@gmail.com>
2017-08-16 17:05:29 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -36,7 +36,7 @@ client.request({url: 'https://public.api.openprocurement.org/api/2.3/contracts?o
 		}
 		}
 
-	if(up>0){
+	//if(up>0){
 	
 		var upDate="";var downDate="";
 		for (var p = 0; p < changeLength; p++) {
@@ -45,9 +45,8 @@ client.request({url: 'https://public.api.openprocurement.org/api/2.3/contracts?o
 		}
 		if(data.getJSON().data.changes[p].rationaleTypes[0]=="priceReduction"){
 			downDate = downDate+data.getJSON().data.changes[p].dateSigned+";"
+		}	
 		}
-		
-	}
 			
 //console.log(changeLength+"-"+upDate);
 	
@@ -85,13 +84,13 @@ client.request({url: 'https://public.api.openprocurement.org/api/2.3/contracts?o
 	
 	//////////SQLite//////////////
 	db.serialize(function() {	
-	db.run("CREATE TABLE IF NOT EXISTS data (dateModified TEXT,dateSigned TEXT,first TEST,tenderID TEXT,procuringEntity TEXT,numberOfBids INT,startAmount INT,amount INT,cpv TEXT,region TEXT,up INT,down INT,upDate TEXT,downDate TEXT)");
+	db.run("CREATE TABLE IF NOT EXISTS data (dateModified TEXT,dateSigned TEXT,first TEXT,tenderID TEXT,procuringEntity TEXT,numberOfBids INT,startAmount INT,amount INT,cpv TEXT,region TEXT,up INT,down INT,upDate TEXT,downDate TEXT)");
 	var statement = db.prepare("INSERT INTO data VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)"); 	
 	statement.run(item.dateModified,dateSigned,first,data.getJSON().data.tenderID,data.getJSON().data.procuringEntity.name,data.getJSON().data.numberOfBids,startAmount,amount,data.getJSON().data.items[0].classification.description,region,up,down,upDate,downDate);
 	//console.log(change);
 	statement.finalize();
 	});
-	}		
+	//}		
 //////////SQLite//////////////	
 	})
 	.catch(function  (error) {
