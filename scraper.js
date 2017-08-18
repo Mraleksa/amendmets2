1a65dcbf9af09598d8e780a0c6914dc44eb061bb
Mraleksa <a.gribanovskiy@gmail.com>
2017-08-18 16:26:10 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -5,7 +5,7 @@ var db = new sqlite3.Database("data.sqlite");
 
 
  
-var start =  "2017-08-16T17:07:03.292060+03:00"
+var start =  "2017-08-16T18:39:02.255100+03:00"
 console.log("старт: "+start); 
 var p=0; var p2=0;
 
@@ -125,7 +125,7 @@ client.request({url: 'https://public.api.openprocurement.org/api/2.3/contracts?o
 		//console.log("error_detale3")				
 	})
 	.then(function () {	
-		if (p<3) {
+		if (p<7) {
 		//piv ();
 		setTimeout(function() {piv ();},20000);
 		}	
