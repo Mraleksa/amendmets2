73107420568d2a8329c8c3e43f465625773bc3e0
Mraleksa <a.gribanovskiy@gmail.com>
2017-08-20 23:07:14 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -5,9 +5,14 @@ var db = new sqlite3.Database("data.sqlite");
 
 
  
-var start =  "2017-04-28T11:32:33.659419+03:00"
-console.log("старт: "+start); 
+//var start =  "2017-03-06T15:10:07.044730+02:00"
+//console.log("старт: "+start); 
 var p=0; var p2=0;
+
+db.each("SELECT dateModified FROM data ORDER BY dateModified DESC LIMIT 1", function(err, timeStart) {
+
+var start = timeStart.dateModified
+console.log("старт: "+timeStart); 
 
 function piv(){  
 p++;
@@ -125,15 +130,15 @@ client.request({url: 'https://public.api.openprocurement.org/api/2.3/contracts?o
 		//console.log("error_detale3")				
 	})
 	.then(function () {	
-		if (p<10) {
-			setTimeout(function() {piv ();},12000);
+		if (p<5) {
+			setTimeout(function() {piv ();},10000);
 		}	
 		else {
 			console.log("stop"+": "+p2)
 			p=0;
 			p2++;
-			if (p2 < 100) {
-				setTimeout(function() {piv ();},25000);
+			if (p2 < 3) {
+				setTimeout(function() {piv ();},20000);
 			}
 			else {
 				console.log("STOP")
@@ -147,4 +152,11 @@ client.request({url: 'https://public.api.openprocurement.org/api/2.3/contracts?o
 	});   					
 }
 
+
+
+
+
+
 piv ();	
+
+})
